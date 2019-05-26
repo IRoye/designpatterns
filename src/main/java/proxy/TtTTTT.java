22880791ffc7eeb5b049906c073f0556b244223e
IRoye <yuyi@newdt.cn>
2019-05-26 15:36:24 +0800

    添加gitignore 配置

---
+++ src/main/java/proxy/TtTTTT.java
@@ -0,0 +1,8 @@
+package proxy;
+
+public class TtTTTT {
+    public static void main(String[] args) {
+        String key = "shinadata";
+        System.out.println(Integer.toBinaryString(key.hashCode()));
+    }
+}
